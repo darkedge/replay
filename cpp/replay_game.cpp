f7605269c5aa0946f16d608fdbcd5749957c22f7
Marco Jonkers <contact@marcojonkers.com>
2016-10-14 17:05:07 +0200

    Sprite creation

--- cpp/replay_game.cpp
+++ cpp/replay_game.cpp
@@ -1,39 +1,72 @@
 #include "replay.h"
-#include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/Graphics/Texture.hpp>
-#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window/Event.hpp>
 #include <assert.h>
 
 // Zero-initialized
 struct GameState {
 	bool initialized;
-	sf::Texture texture1;
-	sf::Sprite sprite1;
+	sf::Sprite* sprite1;
 	float time;
+
+	// Render data
+	sf::Texture textures[256];
+	unsigned int numTextures;
+	sf::Sprite sprites[256];
+	unsigned int numSprites;
+	sf::Text texts[16];
+	unsigned int numTexts;
 };
+
+static sf::Sprite* CreateSprite(GameState* gameState, const char* filename) {
+	// Create texture
+	sf::Texture* texture = gameState->textures + gameState->numTextures++;
+	new (texture) sf::Texture();
+	texture->loadFromFile(filename);
+	texture->setSmooth(true);
+
+	// Create sprite
+	sf::Sprite* sprite = gameState->sprites + gameState->numSprites++;
+	new (sprite) sf::Sprite(*texture);
+	sprite->setOrigin(sf::Vector2f(texture->getSize()) / 2.0f);
+
+	return sprite;
+}
 
 // No static variables here, everything is in GameState
 
 static void Init(GameState* gameState, sf::RenderWindow* window) {
-	new (&gameState->texture1) sf::Texture();
-    if (!gameState->texture1.loadFromFile("../assets/test.png")) {
-        return;
-    }
-	new (&gameState->sprite1) sf::Sprite(gameState->texture1);
-    gameState->sprite1.setOrigin(sf::Vector2f(gameState->texture1.getSize()) / 2.f);
-    gameState->sprite1.setPosition(gameState->sprite1.getOrigin());
-    gameState->sprite1.setScale(5.0f, 5.0f);
+	gameState->sprite1 = CreateSprite(gameState, "../assets/PNG/ufoBlue.png");
+    gameState->sprite1->setPosition(gameState->sprite1->getOrigin());
+    gameState->sprite1->setScale(1.0f, 1.0f);
 }
 
 MJ_EXPORT(void) UpdateGame(void* memory, sf::RenderWindow* window) {
 	assert(memory);
 	assert(window);
 	GameState* gameState = (GameState*) memory;
+
+	sf::Event event;
+	while (window->pollEvent(event)) {
+		switch (event.type) {
+			case sf::Event::KeyPressed: {
+				gameState->initialized = false;
+				break;
+			}
+		}
+	}
+
 	if (!gameState->initialized) {
 		Init(gameState, window);
 		gameState->initialized = true;
 	}
-	//gameState->time += TICK_TIME;
-	//gameState->sprite1.setRotation(gameState->time * 0.1f);
-	window->draw(gameState->sprite1);
+
+	gameState->time += TICK_TIME;
+	gameState->sprite1->setRotation(gameState->time * 0.5f);
+
+	// Rendering
+	// TODO: Sprite sorting, double pointers
+	for (unsigned int i = 0; i < gameState->numSprites; i++) {
+		window->draw(gameState->sprites[i]);
+	}
 }
