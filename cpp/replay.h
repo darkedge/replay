6d14091f274a97d2d77795917b1f986f18bc57eb
Marco Jonkers <contact@marcojonkers.com>
2016-10-25 17:13:47 +0200

    Redefine tick rate

--- cpp/replay.h
+++ cpp/replay.h
@@ -1,7 +1,15 @@
 #pragma once
 
-#define TICK_RATE (5)
-#define TICK_TIME (1.0f / TICK_RATE)
+// Specify tick rate in ms
+// 15 ms = 66.66
+// 30 ms = 33.33
+// 60 ms = 16.66
+// 120 ms = 8.33
+#define TICK_TIME 0.120f
+
+// Specify tick rate in ticks
+//#define TICK_RATE 20
+//#define TICK_TIME (1.0f / TICK_RATE)
 
 #ifdef _MSC_VER
   #define MJ_EXPORT(X) extern "C" __declspec(dllexport) X __cdecl
