92dbebbcd8ea35f86feb95708eb2a7a2bfaea973
Marco Jonkers <contact@marcojonkers.com>
2016-10-26 17:08:59 +0200

    Move tick rate defines to replay_game.h

--- cpp/replay_game.h
+++ cpp/replay_game.h
@@ -3,6 +3,17 @@
 #include "mj_controls.h"
 #include <stdint.h>
 
+// Specify tick rate in ms
+// 15 ms = 66.66
+// 30 ms = 33.33
+// 60 ms = 16.66
+// 120 ms = 8.33
+#define TICK_TIME 0.120f
+
+// Specify tick rate in ticks
+//#define TICK_RATE 20
+//#define TICK_TIME (1.0f / TICK_RATE)
+
 namespace sf {
 	class RenderWindow;
 }
